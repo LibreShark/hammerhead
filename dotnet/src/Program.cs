16cc96fa8d05f0e78ab30fa00b1afd0ebb097885
CheatoBaggins <cheatobaggins@gmail.com>
2023-05-11 00:06:50 -0500

    Add CLI commands: copy-cheats, import-cheats, export-cheats

--- dotnet/src/Program.cs
+++ dotnet/src/Program.cs
@@ -1,47 +1,99 @@
-﻿using System.Collections;
-
-namespace LibreShark.Hammerhead;
+﻿namespace LibreShark.Hammerhead;
 
 using N64;
 
 internal static class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        CopyRomData();
-        ImportCheats();
-        ExportCheats(args);
-    }
-
-    private static void CopyRomData()
-    {
-        var srcPath = "~/dev/libreshark/sharkdumps/n64/firmware/gs-2.00-19980305-pristine.bin";
-        var destPath = "~/dev/libreshark/sharkdumps/n64/firmware/gs-1.09-19980105-pristine.bin";
-
-        var srcBytes = File.ReadAllBytes(srcPath);
-        var destBytes = File.ReadAllBytes(destPath);
-
-        for (var i = 0x0002E000; i < 0x00039000; i++)
+        if (args.Length < 1)
         {
-            destBytes[i] = srcBytes[i];
+            ShowUsage();
+            return 1;
         }
 
-        File.WriteAllBytes(destPath, destBytes);
+        var cmd = args[0];
+        if (cmd == "copy-cheats")
+        {
+            if (args.Length < 3)
+            {
+                ShowUsage();
+                return 1;
+            }
+            return CopyGameList(args[1], args[2]);
+        }
+        if (cmd == "import-cheats")
+        {
+            if (args.Length < 3)
+            {
+                ShowUsage();
+                return 1;
+            }
+            return ImportCheats(args[1], args[2]);
+        }
+        if (cmd == "export-cheats")
+        {
+            if (args.Length < 2)
+            {
+                ShowUsage();
+                return 1;
+            }
+            return ExportCheats(args.Skip(1));
+        }
+
+        ShowUsage();
+        return 1;
     }
 
-    private static void ImportCheats()
+    private static int CopyGameList(string fromRomFilePath, string toRomFilePath)
+    {
+        var srcBytes = File.ReadAllBytes(fromRomFilePath);
+        var destBytes = File.ReadAllBytes(toRomFilePath);
+
+        var srcInfo = RomReader.FromBytes(srcBytes);
+        var destInfo = RomReader.FromBytes(destBytes);
+
+        if (srcInfo == null)
+        {
+            Console.Error.Write($"Invalid GS ROM file source: \"{fromRomFilePath}\"");
+            return 1;
+        }
+        if (destInfo == null)
+        {
+            Console.Error.Write($"Invalid GS ROM file destination: \"{toRomFilePath}\"");
+            return 1;
+        }
+
+        for (var i = 0; i < 0x00010000; i++)
+        {
+            var to = destInfo.GameListOffset + i;
+            var from = srcInfo.GameListOffset + i;
+            if (to > 0x00040000 || from > 0x00040000)
+            {
+                break;
+            }
+            destBytes[to] = srcBytes[from];
+        }
+
+        File.WriteAllBytes(toRomFilePath, destBytes);
+
+        return 0;
+    }
+
+    private static int ImportCheats(string fromDatelFormattedTextFilePath, string toGsRomFilePath)
     {
         Examples.ImportGameListFromFile(
-            "/private/var/folders/7c/x55kqht93czbz4wls69ml0v00000gn/T/gs/gs-2.10-19980825-PRISTINE.txt",
-            "~/dev/libreshark/sharkdumps/n64/firmware/gs-2.10-19980825-ALMOST-PRISTINE.bin"
+            fromDatelFormattedTextFilePath,
+            toGsRomFilePath
         );
+        return 0;
     }
 
-    private static void ExportCheats(string[] args)
+    private static int ExportCheats(IEnumerable<string> romFilePaths)
     {
         var cheatFilePaths = new List<string>();
 
-        foreach (var romFilePath in args)
+        foreach (var romFilePath in romFilePaths)
         {
             RomInfo? romInfo = RomReader.FromBytes(File.ReadAllBytes(romFilePath));
             if (romInfo == null)
@@ -90,5 +142,22 @@ internal static class Program
         {
             Console.WriteLine(cheatFilePath);
         }
+
+        return 0;
+    }
+
+    private static void ShowUsage()
+    {
+        Console.WriteLine(@"
+Usage: dotnet run --project dotnet/src/src.csproj -- COMMAND [...args]
+
+Commands:
+
+    copy-cheats FROM_GS_ROM.bin TO_GS_ROM.bin
+
+    import-cheats FROM_DATEL_FORMATTED.txt TO_GS_ROM.bin
+
+    export-cheats GSROM1.bin GSROM2.z64 GSROM3.n64 ...
+");
     }
 }
