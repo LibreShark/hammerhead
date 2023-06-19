7c76c7a76ec6f76f6965375932e94254bbd6dc09
CheatoBaggins <cheatobaggins@gmail.com>
2023-06-19 16:18:21 -0500

    fmt

--- dotnet/src/Roms/N64XpRom.cs
+++ dotnet/src/Roms/N64XpRom.cs
@@ -7,13 +7,14 @@ using LibreShark.Hammerhead.N64;
 namespace LibreShark.Hammerhead.Roms;
 
 // ReSharper disable BuiltInTypeReferenceStyle
-
 using u8 = Byte;
 using s8 = SByte;
 using s16 = Int16;
 using u16 = UInt16;
 using s32 = Int32;
 using u32 = UInt32;
+using s64 = Int64;
+using u64 = UInt64;
 using f64 = Double;
 
 /// <summary>
