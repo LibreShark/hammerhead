7e9ab82f8d29b2b14c7e3589b2fc178647a0a618
CheatoBaggins <cheatobaggins@gmail.com>
2023-06-20 00:26:17 -0500

    Improve detection of scrambled Xplorer 64 ROM files

--- dotnet/src/Roms/N64XpRom.cs
+++ dotnet/src/Roms/N64XpRom.cs
@@ -327,31 +327,33 @@ public sealed class N64XpRom : Rom
     {
         byte[] idBytes = bytes[0x40..0x55];
         string idStr = idBytes.ToAsciiString();
-        return idStr == "FUTURE CONSOLE DESIGN";
+        return idStr is
+            "Future Console Design" or
+            "FUTURE CONSOLE DESIGN";
+    }
+
+    private static string SS(byte[] bytes, u32 addr)
+    {
+        return bytes[(int)(addr)..(int)(addr + 2)].ToAsciiString();
     }
 
     private static bool DetectScrambled(byte[] bytes)
     {
-        byte[] maybeScrambledFcdBytes =
-        {
-            bytes[0x131E], bytes[0x131F],
-            bytes[0x133E], bytes[0x133F],
-            bytes[0x171E], bytes[0x171F],
-            bytes[0x167E], bytes[0x167F],
-            bytes[0x031E], bytes[0x031F],
-            bytes[0x033E], bytes[0x033F],
-            bytes[0x071E], bytes[0x071F],
-            bytes[0x073E], bytes[0x073F],
-            bytes[0x139E], bytes[0x139F],
-            bytes[0x13BE], bytes[0x13BF],
-            bytes[0x179E],
-        };
-        byte[] expectedFcdBytes = "FUTURE CONSOLE DESIGN"u8.ToArray();
-        bool isFirstEqual = expectedFcdBytes[0x00..0x05]
-            .SequenceEqual(maybeScrambledFcdBytes[0x00..0x05]);
-        bool isSecondEqual = expectedFcdBytes[0x08..0x14]
-            .SequenceEqual(maybeScrambledFcdBytes[0x08..0x14]);
-        return isFirstEqual && isSecondEqual;
+        string strle = SS(bytes, 0x0016);
+        string strFC = SS(bytes, 0x0436);
+        string strDS = SS(bytes, 0x1096);
+        string strXp = SS(bytes, 0x121C);
+        string strlo = SS(bytes, 0x123C);
+        string strFU = SS(bytes, 0x131E);
+        string strTU = SS(bytes, 0x133E);
+        return strle == "le" &&
+               strFC == "FC" &&
+               strDS == "D " &&
+               strXp == "Xp" &&
+               strlo == "lo" &&
+               strFU is "Fu" or "FU" &&
+               strTU is "tu" or "TU" &&
+               true;
     }
 
     public static bool Is(byte[] bytes)
