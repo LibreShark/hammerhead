LibreShark/hammerhead

commit 27bc0f14f8dee4aafdba5ab565208bf039385c25

Refactor

dotnet/src/Roms/GbaGsDatelRom.cs
@@ -1,3 +1,5 @@
+using LibreShark.Hammerhead.IO;
+
 namespace LibreShark.Hammerhead.Roms;
 
 /// <summary>
@@ -10,7 +12,7 @@ public sealed class GbaGsDatelRom : Rom
     private const RomFormat ThisRomFormat = RomFormat.GbaGamesharkDatel;
 
     public GbaGsDatelRom(string filePath, byte[] bytes)
-        : base(filePath, bytes, ThisConsole, ThisRomFormat)
+        : base(filePath, bytes, new LittleEndianScribe(bytes), ThisConsole, ThisRomFormat)
     {
         var minorVersionNumber = Bytes[0x21004];
         var majorVersionNumber = Bytes[0x21005];

commit bccd7698d0f2527b19e9571ddebc5a1ff021ce18

Fix regression introduced in commit 27bc0f14f

dotnet/src/IO/BigEndianScribe.cs
@@ -1,8 +1,5 @@
 ﻿// bacteriamage.wordpress.com
 
-using System.Text;
-using Google.Protobuf;
-
 namespace LibreShark.Hammerhead.IO;
 
 // ReSharper disable BuiltInTypeReferenceStyle
@@ -23,24 +20,24 @@ using f64 = Double;
 /// </summary>
 internal class BigEndianScribe : BinaryScribe
 {
-    public BigEndianScribe(byte[] buffer) : base(buffer)
+    public BigEndianScribe(u8[] bufferRef) : base(bufferRef)
     {
     }
 
     public override u16 ReadU16()
     {
-        byte hi = ReadU8();
-        byte lo = ReadU8();
+        u8 hi = ReadU8();
+        u8 lo = ReadU8();
         s32 value = (hi << 8) + lo;
-        return (u16) value;
+        return (u16)value;
     }
 
     public override BinaryScribe WriteU16(u16 value)
     {
-        byte hi = (byte)((value >> 8) & 0xFF);
-        byte lo = (byte)(value & 0xFF);
-        Buffer[Position++] = hi;
-        Buffer[Position++] = lo;
+        s32 hi = (value >> 8) & 0xFF;
+        s32 lo = value & 0xFF;
+        BufferRef[Position++] = (u8)hi;
+        BufferRef[Position++] = (u8)lo;
         return this;
     }
 
@@ -48,16 +45,15 @@ internal class BigEndianScribe : BinaryScribe
     {
         u32 hi = ReadU16();
         u32 lo = ReadU16();
-
         return (hi << 16) + lo;
     }
 
     public override BinaryScribe WriteU32(u32 value)
     {
-        u16 hi = (u16)(value >> 16);
-        u16 lo = (u16)(value & 0xFFFF);
-        WriteU16(hi);
-        WriteU16(lo);
+        u32 hi = (value >> 16);
+        u32 lo = (value & 0xFFFF);
+        WriteU16((u16)hi);
+        WriteU16((u16)lo);
         return this;
     }
 }

dotnet/src/IO/BinaryScribe.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using System.Text;
 using Google.Protobuf;
 
@@ -16,20 +17,22 @@ using f64 = Double;
 
 public abstract class BinaryScribe
 {
-    protected delegate u16 U16Reader();
-    protected delegate u16 U32Reader();
+    protected readonly u8[] BufferRef;
 
-    protected readonly u8[] Buffer;
-
-    protected BinaryScribe(byte[] buffer)
+    protected BinaryScribe(byte[] bufferRef)
     {
-        Buffer = buffer;
+        BufferRef = bufferRef;
+    }
+
+    public u8[] GetBufferCopy()
+    {
+        return BufferRef.ToArray();
     }
 
     #region Seeking
 
     public u32 Position { get; protected set; }
-    public bool EndReached => Position >= Buffer.Length;
+    public bool EndReached => Position >= BufferRef.Length;
 
     public BinaryScribe Seek(u32 addr)
     {
@@ -48,13 +51,13 @@ public abstract class BinaryScribe
 
     protected void CheckBounds()
     {
-        if (Position == Buffer.Length)
+        if (Position == BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"End of buffer reached: {Buffer.Length} (0x{Buffer.Length:X8})");
+            throw new IndexOutOfRangeException($"End of buffer reached: {BufferRef.Length} (0x{BufferRef.Length:X8})");
         }
-        if (Position > Buffer.Length)
+        if (Position > BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"Invalid position: {Position} (0x{Position:X8}). Must be between 0 and {Buffer.Length} (0x{Buffer.Length:X8}).");
+            throw new IndexOutOfRangeException($"Invalid position: {Position} (0x{Position:X8}). Must be between 0 and {BufferRef.Length} (0x{BufferRef.Length:X8}).");
         }
     }
 
@@ -64,22 +67,22 @@ public abstract class BinaryScribe
 
     public s32 Find(string needle)
     {
-        return Buffer.Find(needle);
+        return BufferRef.Find(needle);
     }
 
     public s32 Find(byte[] needle)
     {
-        return Buffer.Find(needle);
+        return BufferRef.Find(needle);
     }
 
     public bool Contains(string needle)
     {
-        return Buffer.Contains(needle);
+        return BufferRef.Contains(needle);
     }
 
     public bool Contains(byte[] needle)
     {
-        return Buffer.Contains(needle);
+        return BufferRef.Contains(needle);
     }
 
     #endregion
@@ -109,15 +112,15 @@ public abstract class BinaryScribe
 
     public u8[] PeekBytesAt(u32 addr, u32 count)
     {
-        if (addr == Buffer.Length)
+        if (addr == BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"End of buffer reached: {Buffer.Length} (0x{Buffer.Length:X8})");
+            throw new IndexOutOfRangeException($"End of buffer reached: {BufferRef.Length} (0x{BufferRef.Length:X8})");
         }
-        if (addr + count > Buffer.Length)
+        if (addr + count > BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"Invalid position: {addr+count} (0x{addr+count:X8}). Must be between 0 and {Buffer.Length} (0x{Buffer.Length:X8}).");
+            throw new IndexOutOfRangeException($"Invalid position: {addr+count} (0x{addr+count:X8}). Must be between 0 and {BufferRef.Length} (0x{BufferRef.Length:X8}).");
         }
-        return Buffer.Skip((int)addr).Take((int)count).ToArray();
+        return BufferRef.Skip((int)addr).Take((int)count).ToArray();
     }
 
     public u8[] PeekBytes(u32 count)
@@ -138,16 +141,16 @@ public abstract class BinaryScribe
 
     public u8 ReadU8()
     {
-        if (Position == Buffer.Length)
+        if (Position == BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"End of buffer reached: {Buffer.Length} (0x{Buffer.Length:X8})");
+            throw new IndexOutOfRangeException($"End of buffer reached: {BufferRef.Length} (0x{BufferRef.Length:X8})");
         }
-        if (Position > Buffer.Length)
+        if (Position > BufferRef.Length)
         {
-            throw new IndexOutOfRangeException($"Invalid position: {Position} (0x{Position:X8}). Must be between 0 and {Buffer.Length} (0x{Buffer.Length:X8}).");
+            throw new IndexOutOfRangeException($"Invalid position: {Position} (0x{Position:X8}). Must be between 0 and {BufferRef.Length} (0x{BufferRef.Length:X8}).");
         }
 
-        return Buffer[Position++];
+        return BufferRef[Position++];
     }
 
     public s8 ReadS8()
@@ -157,7 +160,7 @@ public abstract class BinaryScribe
 
     public BinaryScribe WriteU8(u8 value)
     {
-        Buffer[Position++] = value;
+        BufferRef[Position++] = value;
         return this;
     }
 
@@ -242,7 +245,7 @@ public abstract class BinaryScribe
         u32 startPos = Position;
         while (true)
         {
-            byte b = Buffer[Position];
+            byte b = BufferRef[Position];
             bool isNull = b == 0;
             u32 bytesRead = Position - startPos;
 
@@ -284,7 +287,7 @@ public abstract class BinaryScribe
                 StartIndex = startPos,
                 EndIndex = endPos,
                 Length = len,
-                RawBytes = ByteString.CopyFrom(Buffer[(int)startPos..(int)endPos]),
+                RawBytes = ByteString.CopyFrom(BufferRef[(int)startPos..(int)endPos]),
             },
             Value = sb.ToString(),
         };

dotnet/src/IO/LittleEndianScribe.cs
@@ -20,7 +20,7 @@ using f64 = Double;
 /// </summary>
 internal class LittleEndianScribe : BinaryScribe
 {
-    public LittleEndianScribe(u8[] buffer) : base(buffer)
+    public LittleEndianScribe(u8[] bufferRef) : base(bufferRef)
     {
     }
 
@@ -36,8 +36,8 @@ internal class LittleEndianScribe : BinaryScribe
     {
         u8 hi = (u8)((value >> 8) & 0xFF);
         u8 lo = (u8)(value & 0xFF);
-        Buffer[Position++] = lo;
-        Buffer[Position++] = hi;
+        BufferRef[Position++] = lo;
+        BufferRef[Position++] = hi;
         return this;
     }
 

dotnet/src/N64/N64GsCrypter.cs
@@ -24,32 +24,30 @@ using f64 = Double;
 /// </summary>
 class N64GsCrypter
 {
-    public static byte[] Encrypt(byte[] input)
+    public static byte[] Encrypt(IReadOnlyList<byte> input)
     {
         return ProcessRom(input, encoder => encoder.Encode());
     }
 
-    public static byte[] Decrypt(byte[] input)
+    public static byte[] Decrypt(IReadOnlyList<byte> input)
     {
         return ProcessRom(input, decoder => decoder.Decode());
     }
 
-    private static byte[] ProcessRom(byte[] input, Action<N64GsCrypter> action)
+    private static byte[] ProcessRom(IReadOnlyList<byte> input, Action<N64GsCrypter> action)
     {
-        N64GsCrypter crypter = new N64GsCrypter(input);
+        var crypter = new N64GsCrypter(input);
         action(crypter);
-        return crypter._output;
+        return crypter._writer.GetBufferCopy();
     }
 
-    private readonly byte[] _output;
     private readonly BigEndianScribe _reader;
     private readonly BigEndianScribe _writer;
 
-    private N64GsCrypter(byte[] input)
+    private N64GsCrypter(IReadOnlyList<byte> input)
     {
         _reader = new BigEndianScribe(input.ToArray());
-        _output = input.ToArray();
-        _writer = new BigEndianScribe(_output);
+        _writer = new BigEndianScribe(input.ToArray());
     }
 
     private static IReadOnlyList<u32> Seeds { get; } = new u32[]

dotnet/src/Roms/GbaGsDatelRom.cs
@@ -14,8 +14,8 @@ public sealed class GbaGsDatelRom : Rom
     public GbaGsDatelRom(string filePath, byte[] bytes)
         : base(filePath, bytes, new LittleEndianScribe(bytes), ThisConsole, ThisRomFormat)
     {
-        var minorVersionNumber = Bytes[0x21004];
-        var majorVersionNumber = Bytes[0x21005];
+        var minorVersionNumber = Buffer[0x21004];
+        var majorVersionNumber = Buffer[0x21005];
     }
 
     public static bool Is(byte[] bytes)

dotnet/src/Roms/GbcGsRom.cs
@@ -38,10 +38,10 @@ public sealed class GbcGsRom : Rom
         "Gameshark     V4.2",
     };
 
-    public GbcGsRom(string filePath, byte[] bytes)
-        : base(filePath, bytes, new LittleEndianScribe(bytes), ThisConsole, ThisRomFormat)
+    public GbcGsRom(string filePath, byte[] rawInput)
+        : base(filePath, rawInput, new LittleEndianScribe(rawInput), ThisConsole, ThisRomFormat)
     {
-        Metadata.Brand = IsGs(Bytes) ? RomBrand.Gameshark : IsAr(Bytes) ? RomBrand.ActionReplay : RomBrand.UnknownBrand;
+        Metadata.Brand = IsGs(Buffer) ? RomBrand.Gameshark : IsAr(Buffer) ? RomBrand.ActionReplay : RomBrand.UnknownBrand;
         Metadata.SortableVersion = ReadVersionNumber();
         Metadata.DisplayVersion = $"v{Metadata.SortableVersion:F2}";
         Metadata.LanguageIetfCode = Metadata.Brand switch

dotnet/src/Roms/N64GsRom.cs
@@ -24,6 +24,7 @@ public sealed class N64GsRom : Rom
     private const GameConsole ThisConsole = GameConsole.Nintendo64;
     private const RomFormat ThisRomFormat = RomFormat.N64Gameshark;
 
+    private bool _isEncrypted;
     private bool _isCompressed;
     private readonly bool _isV3Firmware;
     private readonly bool _isV1GameList;
@@ -46,14 +47,9 @@ public sealed class N64GsRom : Rom
     private const u32 HeaderIdAddr       = 0x00000020;
     private const u32 BuildTimestampAddr = 0x00000030;
 
-    public N64GsRom(string filePath, byte[] bytes)
-        : base(filePath, bytes, new BigEndianScribe(bytes), ThisConsole, ThisRomFormat)
+    public N64GsRom(string filePath, u8[] rawInput)
+        : base(filePath, rawInput, Decrypt(rawInput), ThisConsole, ThisRomFormat)
     {
-        if (IsFileEncrypted())
-        {
-            Decrypt();
-        }
-
         // TODO(CheatoBaggins): Decompress v3.x ROM files
 
         _headerId = Scribe.Seek(HeaderIdAddr).ReadCStringUntilNull(0x10, false);
@@ -72,6 +68,7 @@ public sealed class N64GsRom : Rom
         Metadata.IsKnownVersion = _version.IsKnown;
         Metadata.LanguageIetfCode = _version.Locale.Name;
 
+        _isEncrypted         = DetectEncrypted(rawInput);
         _isV3Firmware        = Scribe.Seek(0x00001000).ReadU32() == 0x00000000;
         _isV1GameList        = Scribe.Seek(0x0002DFF0).ReadU32() == 0x00000000;
         _isV3KeyCodeListAddr = Scribe.Seek(0x0002FBF0).ReadU32() == 0xFFFFFFFF;
@@ -87,6 +84,22 @@ public sealed class N64GsRom : Rom
         _keyCodes = keyCodes;
 
         Games.AddRange(ReadGames());
+    }
+
+    public override bool IsFileEncrypted()
+    {
+        return _isEncrypted;
+    }
+
+    public override bool IsFirmwareCompressed()
+    {
+        return _isCompressed;
+    }
+
+    public static bool Is(u8[] bytes)
+    {
+        bool is256KiB = bytes.IsKiB(256);
+        return is256KiB && (DetectPlain(bytes) || DetectEncrypted(bytes));
     }
 
     public override bool FormatSupportsFileEncryption()
@@ -160,7 +173,7 @@ public sealed class N64GsRom : Rom
 
     private void ReadCode(Cheat cheat, u8 codeIdx)
     {
-        byte[] bytes = Scribe.ReadBytes(6);
+        u8[] bytes = Scribe.ReadBytes(6);
         cheat.Codes.Add(new Code()
         {
             CodeIndex = codeIdx,
@@ -198,15 +211,15 @@ public sealed class N64GsRom : Rom
 
     private N64KeyCode ReadActiveKeyCode(List<N64KeyCode> keyCodes)
     {
-        byte[] activeCrcBytes = Scribe.PeekBytesAt(ActiveKeyCodeAddr, 8);
-        byte[] activePcBytes = Scribe.PeekBytesAt(ProgramCounterAddr, 4);
+        u8[] activeCrcBytes = Scribe.PeekBytesAt(ActiveKeyCodeAddr, 8);
+        u8[] activePcBytes = Scribe.PeekBytesAt(ProgramCounterAddr, 4);
 
         RomString? name = null;
         if (keyCodes.Count > 0)
         {
             N64KeyCode? activeKeyCode = keyCodes.Find(kc =>
             {
-                byte[] curKeyCodeCrcBytes = kc.Bytes.ToArray()[..8];
+                u8[] curKeyCodeCrcBytes = kc.Bytes.ToArray()[..8];
                 return curKeyCodeCrcBytes.SequenceEqual(activeCrcBytes);
             });
             name = activeKeyCode?.Name;
@@ -222,10 +235,10 @@ public sealed class N64GsRom : Rom
 
     private List<N64KeyCode> ReadKeyCodes()
     {
-        byte[] activePrefix = Scribe.PeekBytesAt(ActiveKeyCodeAddr, 8);
+        u8[] activePrefix = Scribe.PeekBytesAt(ActiveKeyCodeAddr, 8);
 
         Scribe.Seek(_keyCodeListAddr);
-        byte[] listBytes = Scribe.PeekBytes(0xA0);
+        u8[] listBytes = Scribe.PeekBytes(0xA0);
         u32 maxPos = Scribe.Position + (u32)listBytes.Length;
         s32 keyCodeByteLength = listBytes.Find("Mario World 64 & Others");
 
@@ -238,7 +251,7 @@ public sealed class N64GsRom : Rom
         List<N64KeyCode> keyCodes = new();
         while (Scribe.Position <= maxPos)
         {
-            byte[] bytes = Scribe.ReadBytes((u32)keyCodeByteLength);
+            u8[] bytes = Scribe.ReadBytes((u32)keyCodeByteLength);
             RomString name = Scribe.ReadPrintableCString(0x1F, true);
             while (Scribe.PeekBytes(1)[0] == 0)
             {
@@ -280,7 +293,7 @@ public sealed class N64GsRom : Rom
 
     private RomString? ReadTitleVersion(string needle)
     {
-        byte[] haystack = Bytes[..0x30000];
+        u8[] haystack = Buffer[..0x30000];
         s32 titleVersionPos = haystack.Find(needle);
         _isCompressed = titleVersionPos == -1;
         if (_isCompressed)
@@ -294,56 +307,6 @@ public sealed class N64GsRom : Rom
         return Scribe.Seek((u32)titleVersionPos).ReadPrintableCString((u32)needle.Length + 5, true).Trim();
     }
 
-    public override bool IsFileEncrypted()
-    {
-        return DetectEncrypted(InitialBytes.ToArray());
-    }
-
-    public override bool IsFirmwareCompressed()
-    {
-        return _isCompressed;
-    }
-
-    private void Decrypt()
-    {
-        if (!DetectEncrypted(Bytes))
-        {
-            return;
-        }
-
-        byte[] decrypted = N64GsCrypter.Decrypt(Bytes);
-        for (u32 i = 0; i < Bytes.Length; i++)
-        {
-            Bytes[i] = decrypted[i];
-        }
-    }
-
-    public byte[] GetEncrypted()
-    {
-        return N64GsCrypter.Encrypt(Bytes);
-    }
-
-    public static bool Is(byte[] bytes)
-    {
-        bool is256KiB = bytes.IsKiB(256);
-        return is256KiB && (DetectPlain(bytes) || DetectEncrypted(bytes));
-    }
-
-    private static bool DetectPlain(byte[] bytes)
-    {
-        byte[] first4Bytes = bytes[..4];
-        bool isN64 = first4Bytes.SequenceEqual(new byte[] { 0x80, 0x37, 0x12, 0x40 }) ||
-                     first4Bytes.SequenceEqual(new byte[] { 0x80, 0x37, 0x12, 0x00 });
-        const string v1Or2Header = "(C) DATEL D&D ";
-        const string v3ProHeader = "(C) MUSHROOM &";
-        return isN64 && (bytes.Contains(v1Or2Header) || bytes.Contains(v3ProHeader));
-    }
-
-    private static bool DetectEncrypted(byte[] bytes)
-    {
-        return bytes[..7].Contains(new byte[] { 0xAE, 0x59, 0x63, 0x54 });
-    }
-
     public static bool Is(Rom rom)
     {
         return rom.Metadata.Format == ThisRomFormat;
@@ -352,6 +315,29 @@ public sealed class N64GsRom : Rom
     public static bool Is(RomFormat type)
     {
         return type == ThisRomFormat;
+    }
+
+    private static BinaryScribe Decrypt(u8[] input)
+    {
+        u8[] output = DetectEncrypted(input)
+            ? N64GsCrypter.Decrypt(input)
+            : input.ToArray();
+        return new BigEndianScribe(output);
+    }
+
+    private static bool DetectPlain(u8[] bytes)
+    {
+        u8[] first4Bytes = bytes[..4];
+        bool isN64 = first4Bytes.SequenceEqual(new u8[] { 0x80, 0x37, 0x12, 0x40 }) ||
+                     first4Bytes.SequenceEqual(new u8[] { 0x80, 0x37, 0x12, 0x00 });
+        const string v1Or2Header = "(C) DATEL D&D ";
+        const string v3ProHeader = "(C) MUSHROOM &";
+        return isN64 && (bytes.Contains(v1Or2Header) || bytes.Contains(v3ProHeader));
+    }
+
+    private static bool DetectEncrypted(u8[] bytes)
+    {
+        return bytes[..7].Contains(new u8[] { 0xAE, 0x59, 0x63, 0x54 });
     }
 
     protected override void PrintCustomHeader()

dotnet/src/Roms/N64XpRom.cs
@@ -40,13 +40,14 @@ public sealed class N64XpRom : Rom
         "2000-05-06T04:42:59+00:00",
     };
 
-    public N64XpRom(string filePath, byte[] bytes)
-        : base(filePath, bytes, new BigEndianScribe(bytes), ThisConsole, ThisRomFormat)
+    private readonly bool _isScrambled;
+    private readonly bool _hasUserPrefs;
+
+    public N64XpRom(string filePath, u8[] rawInput)
+        : base(filePath, rawInput, Unobfuscate(rawInput), ThisConsole, ThisRomFormat)
     {
-        if (IsFileScrambled())
-        {
-            Unscramble();
-        }
+        _isScrambled = DetectScrambled(rawInput);
+        _hasUserPrefs = Scribe.MaintainPosition(() => !Scribe.Seek(UserPrefsAddr).IsPadding());
 
         Metadata.Brand = RomBrand.Xplorer;
 
@@ -96,6 +97,15 @@ public sealed class N64XpRom : Rom
         ReadUserPrefs();
     }
 
+    private static BinaryScribe Unobfuscate(u8[] rawInput)
+    {
+        u8[] output =
+            DetectScrambled(rawInput)
+                ? N64XpScrambler.UnscrambleXpRom(rawInput)
+                : rawInput.ToArray();
+        return new BigEndianScribe(output);
+    }
+
     public override bool FormatSupportsFileScrambling()
     {
         return true;
@@ -108,12 +118,12 @@ public sealed class N64XpRom : Rom
 
     public override bool IsFileScrambled()
     {
-        return DetectScrambled(InitialBytes.ToArray());
+        return _isScrambled;
     }
 
     public override bool HasUserPrefs()
     {
-        return Scribe.MaintainPosition(() => !Scribe.Seek(UserPrefsAddr).IsPadding());
+        return _hasUserPrefs;
     }
 
     private void ReadUserPrefs()
@@ -175,7 +185,7 @@ public sealed class N64XpRom : Rom
 
                 for (u16 codeIdx = 0; codeIdx < codeCount; codeIdx++)
                 {
-                    byte[] codeBytes = Scribe.ReadBytes(6);
+                    u8[] codeBytes = Scribe.ReadBytes(6);
                     string codeStrOld = codeBytes.ToCodeString(GameConsole.Nintendo64);
                     if (IsCodeEncrypted(codeBytes))
                     {
@@ -194,7 +204,7 @@ public sealed class N64XpRom : Rom
                     }
 
                     var codeScribe = new BigEndianScribe(codeBytes);
-                    byte[] bytes = codeScribe.ReadBytes(6);
+                    u8[] bytes = codeScribe.ReadBytes(6);
                     cheat.Codes.Add(new Code()
                     {
                         CodeIndex = codeIdx,
@@ -220,9 +230,9 @@ public sealed class N64XpRom : Rom
     }
 
     // https://doc.kodewerx.org/hacking_n64.html#xp_encryption
-    private byte[] EncryptCode(byte[] code)
+    private u8[] EncryptCode(u8[] code)
     {
-        return new byte[] {};
+        return new u8[] {};
     }
 
     /// <summary>
@@ -230,7 +240,7 @@ public sealed class N64XpRom : Rom
     ///
     /// From https://doc.kodewerx.org/hacking_n64.html#xp_encryption.
     /// </summary>
-    private static byte[] DecryptCodeMethod1(IReadOnlyList<byte> code)
+    private static u8[] DecryptCodeMethod1(IReadOnlyList<byte> code)
     {
         byte a0 = code[0];
         byte a1 = code[1];
@@ -244,7 +254,7 @@ public sealed class N64XpRom : Rom
         a3 = (byte)((a3 ^ 0x83) - 0x2B);
         d0 = (byte)((d0 ^ 0x84) - 0x2B);
         d1 = (byte)((d1 ^ 0x85) - 0x2B);
-        return new byte[] {a0, a1, a2, a3, d0, d1};
+        return new u8[] {a0, a1, a2, a3, d0, d1};
     }
 
     /// <summary>
@@ -252,7 +262,7 @@ public sealed class N64XpRom : Rom
     ///
     /// From https://doc.kodewerx.org/hacking_n64.html#xp_encryption.
     /// </summary>
-    private static byte[] DecryptCodeMethod2(IReadOnlyList<byte> code)
+    private static u8[] DecryptCodeMethod2(IReadOnlyList<byte> code)
     {
         byte a0 = code[0];
         byte a1 = code[1];
@@ -266,7 +276,7 @@ public sealed class N64XpRom : Rom
         a3 = (byte)((a3 + 0xAB) ^ 0x03);
         d0 = (byte)((d0 + 0xAB) ^ 0x04);
         d1 = (byte)((d1 + 0xAB) ^ 0x05);
-        return new byte[] {a0, a1, a2, a3, d0, d1};
+        return new u8[] {a0, a1, a2, a3, d0, d1};
     }
 
     private static string GetIetfCode(RomString oneLetterLangCode, RomString countryNameRaw)
@@ -283,7 +293,7 @@ public sealed class N64XpRom : Rom
 
     private void ReadBuildDate(out RomString buildDateRaw, out string buildDateIso, out RomString wayneStr)
     {
-        u32 waynePos = (u32)Bytes.Find("Wayne Hughes Beckett!");
+        u32 waynePos = (u32)Buffer.Find("Wayne Hughes Beckett!");
         wayneStr = Scribe.Seek(waynePos).ReadCStringUntilNull();
         u32 buildDatePos = waynePos + 0x40;
         buildDateRaw = Scribe.Seek(buildDatePos).ReadCStringUntilNull();
@@ -321,39 +331,33 @@ public sealed class N64XpRom : Rom
         buildDateIso = buildDateTimeWithTz.ToIsoString();
     }
 
-    private void Unscramble()
-    {
-        byte[] unscrambled = N64XpScrambler.UnscrambleXpRom(Bytes);
-        Array.Copy(unscrambled, Bytes, unscrambled.Length);
-    }
-
-    public byte[] GetPlain()
+    public u8[] GetPlain()
     {
         // Return a copy of the array to prevent the caller from mutating
         // internal state.
-        return Bytes.ToArray();
+        return Buffer.ToArray();
     }
 
-    public byte[] GetScrambled()
+    public u8[] GetScrambled()
     {
-        return N64XpScrambler.ScrambleXpRom(Bytes);
+        return N64XpScrambler.ScrambleXpRom(Buffer);
     }
 
-    private static bool DetectPlain(byte[] bytes)
+    private static bool DetectPlain(u8[] bytes)
     {
-        byte[] idBytes = bytes[0x40..0x55];
+        u8[] idBytes = bytes[0x40..0x55];
         string idStr = idBytes.ToAsciiString();
         return idStr is
             "Future Console Design" or
             "FUTURE CONSOLE DESIGN";
     }
 
-    private static string SS(byte[] bytes, u32 addr)
+    private static string SS(u8[] bytes, u32 addr)
     {
         return bytes[(int)(addr)..(int)(addr + 2)].ToAsciiString();
     }
 
-    private static bool DetectScrambled(byte[] bytes)
+    private static bool DetectScrambled(u8[] bytes)
     {
         string strle = SS(bytes, 0x0016);
         string strFC = SS(bytes, 0x0436);
@@ -372,7 +376,7 @@ public sealed class N64XpRom : Rom
                true;
     }
 
-    public static bool Is(byte[] bytes)
+    public static bool Is(u8[] bytes)
     {
         bool is256KiB = bytes.IsKiB(256);
         return is256KiB && (DetectPlain(bytes) || DetectScrambled(bytes));

dotnet/src/Roms/Rom.cs
@@ -5,20 +5,25 @@ using NeoSmart.PrettySize;
 
 namespace LibreShark.Hammerhead.Roms;
 
+// ReSharper disable BuiltInTypeReferenceStyle
+using u8 = Byte;
+using s8 = SByte;
+using s16 = Int16;
+using u16 = UInt16;
+using s32 = Int32;
+using u32 = UInt32;
+using s64 = Int64;
+using u64 = UInt64;
+using f64 = Double;
+
 public abstract class Rom
 {
-    /// <summary>
-    /// Raw, unaltered bytes that were read in from the ROM file.
-    /// May be encrypted or scrambled, depending on the device.
-    /// </summary>
-    protected readonly ImmutableArray<byte> InitialBytes;
-
     /// <summary>
     /// Plain, unencrypted, unobfuscated bytes.
     /// If the input file is encrypted/scrambled, it must be
     /// decrypted/unscrambled immediately in the subclass constructor.
     /// </summary>
-    protected readonly byte[] Bytes;
+    protected byte[] Buffer => Scribe.GetBufferCopy();
 
     public readonly RomMetadata Metadata;
 
@@ -27,13 +32,11 @@ public abstract class Rom
 
     protected Rom(
         string filePath,
-        byte[] bytes,
+        byte[] rawInput,
         BinaryScribe scribe,
         GameConsole console,
         RomFormat format)
     {
-        InitialBytes = bytes.ToImmutableArray();
-        Bytes = bytes.ToArray();
         Scribe = scribe;
         Metadata = new RomMetadata
         {
@@ -106,8 +109,8 @@ public abstract class Rom
     public void PrintSummary()
     {
         Console.WriteLine();
-        Console.WriteLine($"{Metadata.Format.ToDisplayString()} file with length = 0x{Bytes.Length:X8} " +
-                          $"({Bytes.Length} bytes = {PrettySize.Format(Bytes.Length)}): '{Metadata.FilePath}'");
+        Console.WriteLine($"{Metadata.Format.ToDisplayString()} file with length = 0x{Buffer.Length:X8} " +
+                          $"({Buffer.Length} bytes = {PrettySize.Format(Buffer.Length)}): '{Metadata.FilePath}'");
         Console.WriteLine();
         Console.WriteLine($"Format:              {Metadata.Format.ToDisplayString()}");
         Console.WriteLine($"Console:             {Metadata.Console.ToDisplayString()}");