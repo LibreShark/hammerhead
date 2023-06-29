2c710b74a956f2b2de550d7db16823d74942c052
CheatoBaggins <cheatobaggins@gmail.com>
2023-06-29 09:45:09 -0500

    Rename

--- dotnet/src/N64/N64GbHunterRle01Decoder.cs
+++ dotnet/src/N64/N64GbHunterRle01Decoder.cs
@@ -17,7 +17,7 @@ using f64 = Double;
 /// EXPERIMENTAL (UNTESTED) RLE01 decoder for N64 GB Hunter and Game Booster ROM asset compression.
 /// Adapted from https://segaxtreme.net/threads/action-replay-save-format.24708/post-180339
 /// </summary>
-public class N64GsRle01Decoder
+public class N64GbHunterRle01Decoder
 {
     // 0x002897dc ARP_202C.BIN
     public static s32 DecompressRle01(byte[] src, byte[] dest)
