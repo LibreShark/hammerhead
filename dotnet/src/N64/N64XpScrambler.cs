4834581f50ef7df5247900834435b7cb473400b4
CheatoBaggins <cheatobaggins@gmail.com>
2023-07-01 14:56:26 -0500

    fmt

--- dotnet/src/N64/N64XpScrambler.cs
+++ dotnet/src/N64/N64XpScrambler.cs
@@ -1,11 +1,78 @@
 namespace LibreShark.Hammerhead.N64;
 
+// ReSharper disable BuiltInTypeReferenceStyle
+using u8 = Byte;
+using s8 = SByte;
+using s16 = Int16;
+using u16 = UInt16;
+using s32 = Int32;
+using u32 = UInt32;
+using s64 = Int64;
+using u64 = UInt64;
+using f64 = Double;
+
 /// <summary>
 /// Unscrambles Xplorer 64 ROM files dumped directly from the two EEPROM chips,
 /// and re-scrambles them for flashing directly to the EEPROMs.
 /// </summary>
-public class N64XpScrambler
+public static class N64XpScrambler
 {
+    public static u8[] ScrambleRom(u8[] plainBytes)
+    {
+        u8[] high = new u8[plainBytes.Length / 2];
+        u8[] low = new u8[plainBytes.Length / 2];
+
+        for (int i = 0; i < plainBytes.Length; i += 2)
+        {
+            high[i / 2] = plainBytes[i];
+            low[i / 2] = plainBytes[i + 1];
+        }
+
+        u8[] unscrambledHigh = new u8[plainBytes.Length / 2];
+        u8[] unscrambledLow = new u8[plainBytes.Length / 2];
+
+        for (int i = 0; i < (plainBytes.Length / 2); i++)
+        {
+            int scrambledAddress = UnscrambleAddr(i);
+            unscrambledHigh[i] = high[scrambledAddress];
+            unscrambledLow[i] = low[scrambledAddress];
+        }
+
+        u8[] scrambledBytes = unscrambledHigh
+            .Zip(unscrambledLow, (a, b) => new[] { a, b })
+            .SelectMany(ab => ab)
+            .ToArray();
+        return scrambledBytes;
+    }
+
+    public static u8[] UnscrambleRom(u8[] scrambledBytes)
+    {
+        u8[] high = new u8[scrambledBytes.Length / 2];
+        u8[] low = new u8[scrambledBytes.Length / 2];
+
+        for (int i = 0; i < scrambledBytes.Length; i += 2)
+        {
+            high[i / 2] = scrambledBytes[i];
+            low[i / 2] = scrambledBytes[i + 1];
+        }
+
+        u8[] scrambledHigh = new u8[scrambledBytes.Length / 2];
+        u8[] scrambledLow = new u8[scrambledBytes.Length / 2];
+
+        for (int i = 0; i < (scrambledBytes.Length / 2); i++)
+        {
+            int unscrambledAddress = ScrambleAddr(i);
+            scrambledHigh[i] = high[unscrambledAddress];
+            scrambledLow[i] = low[unscrambledAddress];
+        }
+
+        u8[] plainBytes = scrambledHigh
+            .Zip(scrambledLow, (a, b) => new[] { a, b })
+            .SelectMany(ab => ab)
+            .ToArray();
+        return plainBytes;
+    }
+
     private static int UnscrambleAddr(int addr)
     {
         return (((addr >> 4) & 0x001) | ((addr >> 8) & 0x002) | ((~addr >> 9) & 0x004) | ((addr >> 3) & 0x008) |
@@ -18,61 +85,5 @@ public class N64XpScrambler
         return (((~addr >> 8) & 0x001) | ((~addr >> 5) & 0x006) | ((~addr >> 6) & 0x008) | ((addr << 4) & 0x010) |
                 ((addr >> 6) & 0x020) | ((addr << 3) & 0x040) | ((addr << 2) & 0x080) | ((~addr >> 2) & 0x100) |
                 ((addr << 8) & 0x200) | ((addr << 6) & 0x400) | ((~addr << 9) & 0x800) | (addr & 0x1F000));
-    }
-
-    public static byte[] ScrambleRom(byte[] plainBytes)
-    {
-        byte[] high = new byte[plainBytes.Length / 2];
-        byte[] low = new byte[plainBytes.Length / 2];
-
-        for (int i = 0; i < plainBytes.Length; i += 2)
-        {
-            high[i / 2] = plainBytes[i];
-            low[i / 2] = plainBytes[i + 1];
-        }
-
-        byte[] unscrambledHigh = new byte[plainBytes.Length / 2];
-        byte[] unscrambledLow = new byte[plainBytes.Length / 2];
-
-        for (int i = 0; i < (plainBytes.Length / 2); i++)
-        {
-            int scrambledAddress = UnscrambleAddr(i);
-            unscrambledHigh[i] = high[scrambledAddress];
-            unscrambledLow[i] = low[scrambledAddress];
-        }
-
-        byte[] scrambledBytes = unscrambledHigh
-            .Zip(unscrambledLow, (a, b) => new[] { a, b })
-            .SelectMany(ab => ab)
-            .ToArray();
-        return scrambledBytes;
-    }
-
-    public static byte[] UnscrambleRom(byte[] scrambledBytes)
-    {
-        byte[] high = new byte[scrambledBytes.Length / 2];
-        byte[] low = new byte[scrambledBytes.Length / 2];
-
-        for (int i = 0; i < scrambledBytes.Length; i += 2)
-        {
-            high[i / 2] = scrambledBytes[i];
-            low[i / 2] = scrambledBytes[i + 1];
-        }
-
-        byte[] scrambledHigh = new byte[scrambledBytes.Length / 2];
-        byte[] scrambledLow = new byte[scrambledBytes.Length / 2];
-
-        for (int i = 0; i < (scrambledBytes.Length / 2); i++)
-        {
-            int unscrambledAddress = ScrambleAddr(i);
-            scrambledHigh[i] = high[unscrambledAddress];
-            scrambledLow[i] = low[unscrambledAddress];
-        }
-
-        byte[] plainBytes = scrambledHigh
-            .Zip(scrambledLow, (a, b) => new[] { a, b })
-            .SelectMany(ab => ab)
-            .ToArray();
-        return plainBytes;
     }
 }
